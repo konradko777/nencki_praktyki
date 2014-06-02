c2ceabcd61d7c6b44ef17b5deaabf098364389c1
konradko777 <kociepali@gmail.com>
2014-06-02 21:16:50 +0200

    extracting data from 1 iteration

--- extractDataFromText.py
+++ extractDataFromText.py
@@ -1,7 +1,10 @@
 def readDataFromIteration(nameOfTextFile):
 	textFile = open(nameOfTextFile, 'r')
-	for line in textFile:
-		print line
-
+	values = [None]*4
+	for i in range(0,4):
+		line = textFile.readline()
+		numericValue = line.split('=')[1].strip()
+		values[i] = numericValue
+	print values
 
 readDataFromIteration("iter1.txt")
